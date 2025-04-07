9c8930aecde856ae5b720b02e876125287eb5355
anujseth1 <anujseth184@gmail.com>
2025-04-07 12:35:28 +0530

    Updated data loader to preserve full date-product combinations

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -7,11 +7,25 @@ def load_data():
     date = pd.read_csv("data/d_date.csv")
     return sales, inventory, product, date
 
-def merge_dimensions(f_sales, f_inventory, d_products, d_date):
-    sales_df = f_sales.merge(d_products, on="product_id", how="left").merge(d_date, on="date_id", how="left")
-    inventory_df = f_inventory.merge(d_products, on="product_id", how="left").merge(d_date, on="date_id", how="left")
+# def merge_dimensions(f_sales, f_inventory, d_products, d_date):
+#     sales_df = f_sales.merge(d_products, on="product_id", how="left").merge(d_date, on="date_id", how="left")
+#     inventory_df = f_inventory.merge(d_products, on="product_id", how="left").merge(d_date, on="date_id", how="left")
 
-    sales_df["date"] = pd.to_datetime(sales_df["date"])
-    inventory_df["date"] = pd.to_datetime(inventory_df["date"])
+#     sales_df["date"] = pd.to_datetime(sales_df["date"])
+#     inventory_df["date"] = pd.to_datetime(inventory_df["date"])
+
+#     return sales_df, inventory_df
+
+def merge_dimensions(f_sales, f_inventory, d_products, d_date):
+    d_date["date"] = pd.to_datetime(d_date["date"])
+
+    # Get all date-product combinations
+    date_product_matrix = d_date.merge(d_products, how="cross")
+
+    sales_df = date_product_matrix.merge(f_sales, on=["product_id", "date_id"], how="left")
+    inventory_df = date_product_matrix.merge(f_inventory, on=["product_id", "date_id"], how="left")
+
+    sales_df["quantity_sold"] = sales_df["quantity_sold"].fillna(0)
+    inventory_df["quantity_purchased"] = inventory_df["quantity_purchased"].fillna(0)
 
     return sales_df, inventory_df
